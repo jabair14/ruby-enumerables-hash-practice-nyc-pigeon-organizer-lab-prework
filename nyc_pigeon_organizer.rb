d7a01b09ab0c233bcc13b18555f368ad6638bdde
Jabair Khan <jabairk@gmail.com>
2021-02-10 00:12:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,6 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   data.each_with_object({}) do |(key, value), final_array|
-    final_array[k] = v + 3
+    final_array
   binding.pry 
 end
