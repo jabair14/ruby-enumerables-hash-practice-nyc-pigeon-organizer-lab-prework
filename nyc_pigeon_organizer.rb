c973fe7921235b648e2abf04c4407d8d8b350a07
Jabair Khan <jabairk@gmail.com>
2021-02-10 00:24:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,8 +12,9 @@ def nyc_pigeon_organizer(data)
       end
       if !final_array[name][key]
         !final_array[name][key] = []
+      end
     end 
-    final_array
+    final_array[name][key].push(inner_key)
   end
   binding.pry 
 end
